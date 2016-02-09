825aa05be8c37f84e13bade3a84eb14520b563a9
benhigh22 <benjaming.high@gmail.com>
2016-02-09 14:49:45 -0500

    Construction of initial classes and functions, no values

---
+++ blackjack.py
@@ -0,0 +1,74 @@
+
+
+class Card:
+
+    def __init__(self):
+        pass
+
+    def card_value(self):
+        return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+
+    def card_suit(self):
+        return ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+
+class Deck:
+
+    def __init__(self):
+        pass
+
+    def shuffle(self):
+
+    def deal_a_card(self):
+
+    def full_deck(self):
+
+    def partial_deck(self):
+
+    def new_hand(self):
+
+class Hand:
+
+    def __init__(self):
+        pass
+
+    def dealt_hand(self):
+
+    def value_of_hand(self):
+
+    def show_hand(self):
+
+class House:
+
+    def __init__(self):
+        pass
+
+    def show_house_hand(self):
+
+    def dealt_house_hand(self):
+
+    def hit_house(self):
+
+    def stay_house(self):
+
+    def house_wins(self):
+
+    def house_loses(self):
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def show_player_hand(self):
+
+    def dealt_player_hand(self):
+
+    def hit_player(self):
+
+    def stay_player(self):
+
+    def player_wins(self):
+
+    def player_loses(self):
+        

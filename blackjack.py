f894ca947bdcf7d4f10b1698232c9b9ef4a498bf
benhigh22 <benjaming.high@gmail.com>
2016-02-10 17:02:10 -0500

    Next save

--- blackjack.py
+++ blackjack.py
@@ -1,74 +1,84 @@
-
-
-class Card:
-
-    def __init__(self):
-        pass
-
-    def card_value(self):
-        return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
-
-    def card_suit(self):
-        return ["Hearts", "Diamonds", "Clubs", "Spades"]
-
+import random
 
 class Deck:
 
     def __init__(self):
-        pass
+        self.suit = ["H", "C", "S", "D"]
+        self.val = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
+        self.deck = []
 
-    def shuffle(self):
+    def make_deck(self):
+        for s in self.suit:
+            for v in self.val:
+                self.deck.append(v + s)
+        return self.deck
 
-    def deal_a_card(self):
+    def give_one(self):
+        card = self.deck.pop()
+        return card
 
-    def full_deck(self):
+    def create_hand(self):
+        hand = []
+        for card in range(2):
+            hand.append(self.give_one())
+        return hand
 
-    def partial_deck(self):
 
-    def new_hand(self):
+    def get_hand_value(self, hand):
+       d = 0
+       for card in hand:
+           for value in card[0]:
+               try:
+                   value = int(value)
+                   if int(value) == 1:
+                       value = 10
+               except ValueError:
+                   if value == 'A':
+                       value = 11
+                   elif value in 'JQK':
+                       value = 10
+           d += int(value)
+       return d
 
-class Hand:
 
-    def __init__(self):
-        pass
+deck = Deck()
+deck_creation = deck.make_deck()
+print(deck_creation)
+hit_one = deck.give_one()
+hand_creation = deck.create_hand()
+print(hand_creation)
+hand_value = deck.get_hand_value(hand_creation)
+print(hand_value)
 
-    def dealt_hand(self):
 
-    def value_of_hand(self):
 
-    def show_hand(self):
-
-class House:
-
-    def __init__(self):
-        pass
-
-    def show_house_hand(self):
-
-    def dealt_house_hand(self):
-
-    def hit_house(self):
-
-    def stay_house(self):
-
-    def house_wins(self):
-
-    def house_loses(self):
 
 class Player:
 
     def __init__(self):
-        pass
+        self.hand = hand_creation
+        self.value = hand_value
 
     def show_player_hand(self):
+        return self.hand
 
-    def dealt_player_hand(self):
+    def player_hand(self):
+        return self.value
 
-    def hit_player(self):
+    def player_hit(self):
+        want_hit = input("Do you want to hit? Enter y or n ")
+        while True:
+            if want_hit == "y":
+                print("You've chosen to hit. ")
+                return hit_one
 
-    def stay_player(self):
+            elif want_hit == "n":
+                print("You've chosen to stand. ")
+                break
 
-    def player_wins(self):
 
-    def player_loses(self):
-        
+
+player = Player()
+show_hand_player = player.show_player_hand()
+val_player_hand = player.player_hand()
+print(player.player_hit())

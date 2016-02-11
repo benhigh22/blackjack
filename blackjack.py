c05b07b096e8c2ca16fd00c24e0eb42761b43cc1
benhigh22 <benjaming.high@gmail.com>
2016-02-11 15:37:50 -0500

    Fully working game, not in classes yet

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,17 @@
-
 import random
 
 class Deck:
 
     def __init__(self):
-        self.suit = ["H", "C", "S", "D"]
+        self.suit = ["Hearts", "Clubs", "Spades", "Diamonds"]
         self.val = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
         self.deck = []
 
     def make_deck(self):
         for s in self.suit:
             for v in self.val:
-                self.deck.append(v + s)
-        return self.deck
+                self.deck.append(v + " of " + s)
+        return random.shuffle(self.deck)
 
     def give_player_one(self):
         card = self.deck.pop()
@@ -78,6 +77,7 @@ class Deck:
             except ValueError:
                if value == 'A':
                    value = 11
+
                elif value in 'JQK':
                    value = 10
         d += int(value)
@@ -111,15 +111,13 @@ player_hand_value = deck.get_player_hand_value(player_hand_creation)
 print("Your hand is " + str(player_hand_creation) + " which has a value of " + str(player_hand_value))
 dealer_hand_creation = deck.create_dealer_hand()
 dealer_hand_value = deck.get_dealer_hand_value(dealer_hand_creation)
-new_player_total = player_single_card_val + player_hand_value
-new_dealer_total = dealer_single_card_val + dealer_hand_value
-
+print("Dealer's first card is " + str(dealer_hand_creation[0]))
 
 while True:
     want_hit = input("Do you want to hit? Enter y or n ")
     if want_hit == "n":
         print("You've chosen to stand. ")
-        print(player_hand_value)
+        print("You're total hand value is " + str(player_hand_value))
         break
 
     elif want_hit == "y":
@@ -127,8 +125,8 @@ while True:
         player_single_card_val = deck.get_player_single_value(player_hit_one)
         player_hand_value += player_single_card_val
         print("You've chosen to hit. ")
-        print("You're card is a " + str(player_single_card_val))
-        print (player_hand_value)
+        print("You're card is a " + str(player_hit_one) + " with a value of " + str(player_single_card_val))
+        print ("You're total hand value is " + str(player_hand_value))
         if player_hand_value > 21:
             print("You busted! ")
             break
@@ -137,34 +135,41 @@ while True:
         print("Not a valid choice. Do you want to hit? Enter y or n ")
         continue
 
+
 while True:
+
     if dealer_hand_value >= 17 and dealer_hand_value <= 21:
-        new_dealer_total == dealer_hand_value
         print("Dealer has chosen to stand. ")
         print("Dealer's total is " + str(dealer_hand_value))
         break
-
 
     elif dealer_hand_value < 17:
         dealer_hit_one = deck.give_dealer_one()
         dealer_single_card_val = deck.get_dealer_single_value(dealer_hit_one)
         dealer_hand_value += dealer_single_card_val
         print("Dealer has chosen to hit. ")
-        print("Dealer's new total is " + str(new_dealer_total))
-        break
+        print("Dealer's new total is " + str(dealer_hand_value))
+        if dealer_hand_value >= 17 and dealer_hand_value <= 21:
+            print("Dealer has chosen to stand. ")
+            print("Dealer's total is " + str(dealer_hand_value))
+            break
+        elif dealer_hand_value > 21:
+            print("Dealer busted!")
+            break
 
 def outcome():
-    if new_player_total > 21:
+
+    if player_hand_value > 21:
         return "You lose!"
-    elif new_dealer_total > 21:
+    elif dealer_hand_value > 21:
         return "You win!"
-    elif new_player_total > new_dealer_total:
+    elif player_hand_value > dealer_hand_value:
         return "You win!"
-    elif new_player_total < new_dealer_total:
+    elif player_hand_value < dealer_hand_value:
         return "You lose."
-    elif new_player_total == new_dealer_total:
+    elif player_hand_value == dealer_hand_value:
         return "You pushed with the dealer!"
 
-
+print(outcome())
 
 
